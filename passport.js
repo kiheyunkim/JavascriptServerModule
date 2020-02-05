5188386d0d372786a299b65162ff4c85a823f0c5
kiheyunkim <kiheyunkim@gmail.com>
2020-02-05 16:59:18 +0900

    modify structure

--- passport.js
+++ passport.js
@@ -1,37 +1,42 @@
+const fs = require('fs');
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20').Strategy;
+const passportInfo = JSON.parse(fs.readFileSync(__dirname+ '/../../AuthInfo/passportInfo.json',{encoding:'UTF-8'}));
+//const app = require('express')();
 
 let Addpassport = (app)=>{
-    if(require('express')() === undefined || app !== require('express')() )
-        throw new Error('not express module');
-
     app.use(passport.initialize());
     app.use(passport.session());
 
-    passport.serializeUser((user,done)=>{
-        done(null,user.id);
-    });
-
-    passport.deserializeUser((id,done)=>done(null,id));
-
     passport.use(new googleStrategy({
-        clientID: '----',
-        clientSecret: '----',
-        callbackURL: '----'
+        clientID: passportInfo.CLIENT_ID,
+        clientSecret: passportInfo.CLIENT_SECRET,
+        callbackURL: passportInfo.CALLBACK_URL
     },
-    (accessToken, refreshToken, profile, done)=> {
+    (accessToken, refreshToken, profile, done)=> {      //로그인 되는 순간에 불러온다.
         // asynchronous verification, for effect...
         process.nextTick( ()=>{
-          console.log(profile);
           // To keep the example simple, the user's Google profile is returned to
           // represent the logged-in user.  In a typical application, you would want
           // to associate the Google account with a user record in your database,
           // and return that user instead.
-          return done(null, profile);
+          return done(null, profile.emails);
         });
     }));
 
-    app.get('/login/google',
+    passport.serializeUser((user,done)=>{//값을 가져올떄(로그인 직후의 상황)
+        done(null,user);
+    });
+
+    passport.deserializeUser((user,done)=>{//세션에 저장된 값을 가져올 때
+        done(null,user);
+    });
+}
+
+exports.Addpassport = Addpassport;
+
+/* 아래 형태로 주입
+app.get('/login/google',
         passport.authenticate('google', { scope: [
             'https://www.googleapis.com/auth/userinfo.email'],
             accessType: 'offline', approvalPrompt: 'force'}),
@@ -45,20 +50,4 @@ let Addpassport = (app)=>{
     (req, res)=>{//console.log(req.query);
       res.redirect('/login');
     });
-
-    app.get('/logout/google', function(req, res){
-        req.logout();
-        res.redirect('/login');
-      });
-
-    app.listen('3000');
-
-    app.get('/login',(request,response)=>{
-        response.send(`
-        <a href='/login/google'>로그인</a>
-        <a href='/login/google'>로그아웃</a>
-        `)
-    })
-}
-
-exports.Addpassport = Addpassport;
+*/
